0c485d23fa6abf1a03a5e456609979d780e91f49
David Whitney <dwhitnee@gmail.com>
2016-11-30 23:35:38 -0800

    Day 1 complete
    Fix line crossing and gfx issues (save/restore styles)

--- 2016/day1.js
+++ 2016/day1.js
@@ -7,35 +7,91 @@
 //     console.log( data );
 //   });
 
+// var data = "R8, R4, R4, R8";
 var data = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3";
 
 var gfx;
-var start = { x:200, y:200 };
+var start = { x:200, y:100 };
+var grid = {};
 
 function initGfx() {
   var c = document.getElementById("canvas");
   var gfx = c.getContext("2d");
   gfx.translate( start.x, start.y );
+  // gfx.scale( 10,10 );
+
+  gfx.beginPath();
+  gfx.strokeStyle="#777";
+  gfx.moveTo( 0, 0 );
 
   return gfx;
 }
 
-function moveTo( gfx, pos ) {
+function drawTo( gfx, pos ) {
   gfx.lineTo( pos.x, pos.y );
   gfx.stroke();
 }
 
 function markLocation( gfx, pos, color ) {
+  gfx.save();
   gfx.strokeStyle = color;
   gfx.strokeRect( pos.x-20, pos.y-20, 40, 40);
+  gfx.restore();
 }
 
 function finish( gfx, pos ) {
-  gfx.strokeStyle="#FF0000";
+  gfx.closePath();  // end old line
 
+  gfx.beginPath();
+  gfx.setLineDash([1, 1]);  /*dashes are 5px and spaces are 3px*/
   gfx.moveTo( 0, 0 );
   gfx.lineTo( pos.x, pos.y );
   gfx.stroke();
+}
+
+// @return true if we've been here before
+function haveWeBeenThere( pos ) {
+  var beenThere = grid[pos.x+":"+pos.y];
+  grid[pos.x+":"+pos.y] = true;
+
+  console.log( JSON.stringify( grid ));
+  return beenThere;
+}
+
+function moveX( pos, distance ) {
+  var beenThere = false;
+  var outBunny;
+
+  for (var i=0; i < Math.abs( distance ); i++) {
+    if (distance < 0 ) {
+      pos.x -= 1;
+    } else {
+      pos.x += 1;
+    }
+    beenThere |= haveWeBeenThere( pos );
+    if (!outBunny && beenThere) {
+      outBunny = Object.assign({}, pos);  // shallow clone
+    }
+  }
+  return outBunny;
+}
+
+function moveY( pos, distance ) {
+  var outBunny;
+  var beenThere = false;
+
+  for (var i=0; i < Math.abs( distance ); i++) {
+    if (distance < 0 ) {
+      pos.y -= 1;
+    } else {
+      pos.y += 1;
+    }
+    beenThere |= haveWeBeenThere( pos );
+    if (!outBunny && beenThere) {
+      outBunny = Object.assign({}, pos);  // shallow clone
+    }
+  }
+  return outBunny;
 }
 
 
@@ -43,7 +99,6 @@ function run() {
   var gfx = initGfx();
 
   var moves = data.split(", ");
-  var beenThere = {};
   var doneThat = false;
 
   var dirs = {
@@ -57,45 +112,42 @@ function run() {
   var pos = { x:0, y:0 };
   var shortestDistance = 0;
 
-  markLocation( gfx, pos, "#FFFFFF");
+  markLocation( gfx, pos, "#a0a0a0");
 
   for (var i=0; i < moves.length; i++) {
     var turn = moves[i].slice( 0,1 );
     var distance = parseInt( moves[i].slice( 1 ));
 
     dir = dirs[dir][turn];
+    var bunny = undefined;
 
     if (dir === "N") {
-      pos.x += distance;
+      bunny = moveY( pos, distance );
     } else if (dir == "S") {
-      pos.x -= distance;
+      bunny = moveY( pos, -distance );
     } else if (dir == "W") {
-      pos.y -= distance;
+      bunny = moveX( pos, -distance );
     } else if (dir == "E") {
-      pos.y += distance;
+      bunny = moveX( pos, distance );
     }
-    moveTo( gfx, pos );
+    drawTo( gfx, pos );
 
-    if (beenThere[pos.x+":"+pos.y] && !doneThat) {
-      console.log( JSON.stringify( pos ));
-      console.log( JSON.stringify( beenThere ));
-
-      shortestDistance = Math.abs( pos.x ) + Math.abs( pos.y );
-      $("#answer2").text( shortestDistance );
-      markLocation( gfx, pos, "#00FF00");
+    if (bunny && !doneThat) {
       doneThat = true;
+      shortestDistance = Math.abs( bunny.x ) + Math.abs( bunny.y );
+      $("#answer2").text( shortestDistance );
+      markLocation( gfx, bunny, "#00FF00");
     }
-    beenThere[pos.x+":"+pos.y] = true;
   }
 
+  markLocation( gfx, pos, "#FF0000");
   finish( gfx, pos );
 
-  var shortestDistance = Math.abs( pos.x ) + Math.abs( pos.y );
+  shortestDistance = Math.abs( pos.x ) + Math.abs( pos.y );
 
   console.log("Final postion is " + JSON.stringify( pos ));
   console.log("Shortest distance is " + shortestDistance );
   $("#answer1").text( shortestDistance );
-  markLocation( gfx, pos, "#FF0000");
 }
 
 $( run );
